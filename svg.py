60f9a9a36d273f45d4085d7da0a0ee0e1a6f434c
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-11-12 01:03:19 +0100

    added rotation to rectangle

--- svg.py
+++ svg.py
@@ -16,7 +16,7 @@ class SVG(object):
         templates["finalize"] = "</svg>"
         templates["circle"] = "    <circle stroke='{}' stroke-width='{}px' fill='{}' r='{}' cy='{}' cx='{}' />\n"
         templates["line"] = "    <line stroke='{}' stroke-width='{}px' y2='{}' x2='{}' y1='{}' x1='{}' />\n"
-        templates["rectangle"] = "    <rect fill='{}' stroke='{}' stroke-width='{}px' width='{}' height='{}' y='{}' x='{}' ry='{}' rx='{}' opacity='{}' />\n"
+        templates["rectangle"] = "    <rect fill='{}' stroke='{}' stroke-width='{}px' width='{}' height='{}' y='{}' x='{}' ry='{}' rx='{}' opacity='{}'  transform='rotate{}' />\n"
         templates["text"] = "    <text x='{}' y = '{}' font-family='{}' stroke='{}' fill='{}' font-size='{}px'>{}</text>\n"
         templates["ellipse"] = "    <ellipse cx='{}' cy='{}' rx='{}' ry='{}' fill='{}' stroke='{}' stroke-width='{}' />\n"
         return templates
@@ -36,11 +36,11 @@ class SVG(object):
     def line(self, stroke, strokewidth, x1, y1, x2, y2):
         self.__add_to_svg(self.templates["line"].format(stroke, strokewidth, y2, x2, y1, x1))
 
-    def rectangle(self, width, height, x, y, fill, stroke, strokewidth, radiusx, radiusy, opacity=1):
-        self.__add_to_svg(self.templates["rectangle"].format(fill, stroke, strokewidth, width, height, y, x, radiusy, radiusx, opacity))
+    def rectangle(self, width, height, x, y, fill, stroke, strokewidth, radiusx, radiusy, opacity=1, transform=(0,0,0)):
+        self.__add_to_svg(self.templates["rectangle"].format(fill, stroke, strokewidth, width, height, y, x, radiusy, radiusx, opacity, transform))
 
     def fill(self, Fill):
-        self.rectangle(self.width, self.height, 0, 0, Fill, Fill, 0, 0, 0, 1)
+        self.rectangle(self.width, self.height, 0, 0, Fill, Fill, 0, 0, 0)
 
     def text(self, x, y, fontfamily, fontsize, fill, stroke, text):
         self.__add_to_svg(self.templates["text"].format(x, y, fontfamily, stroke, fill, fontsize, text))
